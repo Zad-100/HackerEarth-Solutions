d0be90f62ae4e1e4bc34792fae78e5017e08defd
Zad-100 <akash2001parida@gmail.com>
2021-04-10 13:27:11 +0530

    Tweaked the whole program to do it by the 'left-right pointer approach'. Ran it successfully.

--- Beginners_07_04/IfSumEqualTo_k.c
+++ Beginners_07_04/IfSumEqualTo_k.c
@@ -1,14 +1,16 @@
 // This program reads a list of numbers and checks if the sum of any two elements
 // is equal to a certain number 'k' input by the user
 //                Written By : Akash Parida
-//                Date : 08/04/21
+//                Date : 10/04/21
 
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
-unsigned int checkTheSums(int list[], int size, int k); // function prototype
+// function prototype
+unsigned int checkTheSums(int list[], int left, int right, int k);
+void bubbleSort(int list[], int size);
 
 // Function main begins program execution
 int main(void)
@@ -27,7 +29,9 @@ int main(void)
     printf("%s", "\nEnter the integer for which you want to check : "); // prompt
     scanf("%d", &k); // read from the user
 
-    unsigned int result = checkTheSums(nums, n, k); // function call
+    bubbleSort(nums, n); // function call to sort the array
+
+    unsigned int result = checkTheSums(nums, 0, (n - 1),  k); // function call
 
     if (result == 1) {
         printf("%s", "\nTrue\n\n");
@@ -39,15 +43,35 @@ int main(void)
 } // end function main
  
 // User-defined function definition
-unsigned int checkTheSums(int list[], int size, int k)
+
+void bubbleSort(int list[], int size)
 {
-    for (size_t i = 0; i <= (size - 1); i++) {
-        for (size_t j = (i + 1); j <= (size - 1); j++) {
-            if (list[i] + list[j] == k) {
-                return(1);
+    for (unsigned int pass = 1; pass <= (size -1); pass++) { // control the number of passes
+        for (size_t i = 0; i <= (size - 2); i++) { // control the number of comparisions per pass
+            if (list[i] >= list[i + 1]) {
+                // swapping the values
+                int hold = list[i];
+                list[i] = list[i + 1];
+                list[i + 1] = hold;
             } // end if
         } // end for
     } // end for
+} // end bubble sort
 
-    return(0);
+unsigned int checkTheSums(int list[], int left, int right, int k)
+{
+    while (left <= right) { // the moment left > right, we have checked for all pairs' sums
+        if (list[left] + list[right] == k) {
+            return(1); // found
+        } // end if
+        else if (list[left] + list[right] < k) {
+            return(checkTheSums(list, (left + 1), right, k)); // move the left pointer to the right by 1
+        } // end else if
+        else
+        {
+            return(checkTheSums(list, left, (right - 1), k)); // move the right pointer to the left by 1
+        } // end else
+    } // end while
+
+    return(0); // not found
 } // end function checkTheSums
